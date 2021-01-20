1e0ac066d7c3f122c3d7c0a20fe77f246db775d6
yruben <y.ruben1901@gmail.com>
2021-01-20 15:23:48 -0500

    Dynamic Ingredients Finished!

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,21 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import styles from './Burger.module.css'
 
 const burger = (props) =>{
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey =>{
+      return [...Array(props.ingredients[ingKey])].map((_,index)=>{
+        return <BurgerIngredient key={ingKey+index} type={ingKey}/>
+      })
+    }).reduce((acc, elem) => {
+      return acc.concat(elem)
+    }, [])
+
+  if(!transformedIngredients.length)
+    transformedIngredients = <p>There are not ingredients to add...</p>
   return(
     <div className={styles.Burger}>
       <BurgerIngredient type='bread-top'/>
-      <BurgerIngredient type='cheese'/>
-      <BurgerIngredient type='meat'/>
+      { transformedIngredients }
       <BurgerIngredient type='bread-bottom'/>
     </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
+  state = {
+    ingredients:{
+      salad:0,
+      meat:0,
+      bacon:0,
+      cheese:0
+    }
+  }
   render() {
     return(
       <>
-        <Burger/>
+        <Burger ingredients = {this.state.ingredients}/>
         <div> Build Controls</div>
       </>
     )
