029dc0cbe5b0967cdffaf24686101d9b237cfc67
yruben <y.ruben1901@gmail.com>
2021-01-20 22:08:24 -0500

    Added the Side Drawer...!!

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import styles from './Layout.module.css'
 
 const layout = ( props ) => (
   <>
     {/*<div>Toolbar, SideDrawer,Backdrop</div>*/}
     <Toolbar />
+    <SideDrawer/>
     <main className={styles.content}>
       {props.children}
     </main>
